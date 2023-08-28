1f1b5902cdcbafceec572c4c945f8b7111eaf699
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-28 21:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+setInterval(()=>{
+let now = new Date();
+let currentTime = now.toLocaleString();
+
+document.getElementById("timer").textContent = currentTime;
+	
+},1000)
